fc5eb1f79f0f5d99c44fdd6843cb276ca2ed6ef1
tarrinros <tarindis@gmail.com>
2019-03-23 21:29:53 +0200

    Add component method to application helper and remove page controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Method for rendering components
+  def component(component_name, locals = {}, &block)
+    name = component_name.split("_").first
+    render("components/#{name}/#{component_name}", locals, &block)
+  end
+
+  # The alias method for 'component'
+  alias c component
 end
